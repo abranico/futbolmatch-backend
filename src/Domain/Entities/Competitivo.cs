ebcd6f85131868c66ca18903ca80130f379397db
ivanapanowicz <ivanapanowicz@gmail.com>
2024-06-05 15:27:47 -0300

    creacion entidad equipo

---
+++ src/Domain/Entities/Competitivo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    internal class Competitivo
+    {
+        public string Liga { get; set; }
+        public string Resultado { get; set; }
+    }
+}
