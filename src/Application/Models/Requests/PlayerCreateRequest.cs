43a93f55aef886090e44d2ff2037181496f1c304
ivanapanowicz <ivanapanowicz@gmail.com>
2024-06-26 10:47:05 -0300

    DTO requests

---
+++ src/Application/Models/Requests/PlayerCreateRequest.cs
@@ -0,0 +1,22 @@
+﻿using Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Models.Requests
+{
+    public class PlayerCreateRequest
+    {
+        public string Email { get; set; }
+        public string Username { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string Password { get; set; }
+        public string Country { get; set; }
+        public string City { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public Gender Gender { get; set; }
+    }
+}
---
+++ src/Application/Models/Requests/PlayerUpdateRequest.cs
@@ -0,0 +1,27 @@
+﻿using Domain.Entities;
+using Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Models.Requests
+{
+    public class PlayerUpdateRequest
+    {
+        public string? Email { get; set; }
+        public string? Username { get; set; }
+        public string? Firstname { get; set; }
+        public string? Lastname { get; set; }
+        public string? Password { get; set; }
+        public string? Country { get; set; }
+        public string? City { get; set; }
+        public DateTime? DateOfBirth { get; set; }
+        public Gender? Gender { get; set; }
+        public string? Description { get; set; }
+        public PreferredFoot? PreferredFoot { get; set; }
+        public Position? Position { get; set; }
+        public Role? Role { get; set; }
+    }
+}
