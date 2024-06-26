f28746ebf77f7caaefc1fbbdf185f94c9daef2c3
ivanapanowicz <ivanapanowicz@gmail.com>
2024-06-26 10:33:19 -0300

    controllers

---
+++ src/Web/Controllers/PlayerController.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Domain.Exceptions;
+using Application.Models.Requests;
+using Application.Interfaces;
+
+namespace Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PlayerController : ControllerBase
+    {
+        private readonly IPlayerService _playerService;
+        public PlayerController(IPlayerService playerService)
+        {
+            _playerService = playerService;
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Ok(_playerService.GetAll());
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            try
+            {
+                return Ok(_playerService.GetById(id));
+
+            } catch (NotFoundException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Create([FromBody] PlayerCreateRequest request)
+        {
+            var obj = _playerService.Create(request);
+            return CreatedAtAction(nameof(GetById), new { id = obj.Id}, obj);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Update([FromRoute] int id, [FromBody] PlayerUpdateRequest request)
+        {
+            try
+            {
+                _playerService.Update(id, request);
+                return NoContent();
+
+            } catch (NotFoundException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete([FromRoute] int id)
+        {
+            try
+            {
+                _playerService.Delete(id);
+                return NoContent();
+
+            }
+            catch (NotFoundException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
